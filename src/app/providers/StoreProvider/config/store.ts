138572f72220f358af1666d7090e1d1470b20845
Alex <shostak-aleshka@mail.ru>
2024-08-03 04:47:04 +0300

    first commit

---
+++ src/app/providers/StoreProvider/config/store.ts
@@ -0,0 +1,29 @@
+import { configureStore, DeepPartial, ReducersMapObject } from '@reduxjs/toolkit';
+import { counterReducer } from 'entities/Counter';
+import { userReducer } from 'entities/User';
+import { StateSchema } from './StateSchema';
+import { createReducerManager } from './reducerManager';
+
+export function createReduxStore(
+    initialState?: StateSchema,
+    asyncReducers?: ReducersMapObject<StateSchema>,
+) {
+    const rootReducers: ReducersMapObject<StateSchema> = {
+        ...asyncReducers,
+        counter: counterReducer,
+        user: userReducer,
+    };
+
+    const reducerManager = createReducerManager(rootReducers);
+
+    const store = configureStore<StateSchema>({
+        reducer: reducerManager.reduce,
+        devTools: __IS_DEV__,
+        preloadedState: initialState,
+    });
+
+    // @ts-ignore
+    store.reducerManager = reducerManager;
+
+    return store;
+}
